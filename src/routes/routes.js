a752dbfcc8e9a3944ffab418cdc10fde457e45ef
David Uribe <uribesotod@gmil.com>
2017-12-22 13:02:02 -0500

    paginacion lista

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -11,6 +11,38 @@ router.get('/add-product',(req,res)=>{
     res.render('products/add-product');
 });
 
+router.get('/products/:page',(req,res,next)=>{
+    let perPage = 6;
+    let page = req.params.page || 1;
+
+    Product.find({})
+    .skip((perPage * page) -perPage)
+    .limit(perPage)
+    .exec((err,productos)=>{
+        Product.count((err,cuenta)=>{
+            if(err){return next(err)};
+            res.render('products/products',{
+                productos,
+                current: page,
+                pages: Math.ceil(cuenta/perPage)
+            });
+        })
+    })
+});
+
+router.post('/add-product',(req,res)=>{
+    const product = new Product();
+    product.category = req.body.category_name;
+    product.name = req.body.product_name;
+    product.price = req.body.product_price;
+    product.cover = faker.image.image();
+    product.save(err=>{
+        if(err){return next(err)}
+        res.redirect('/add-product');
+    });
+
+});
+
 router.get('/generate-fake-data',(req, res)=>{
     for(let i=0; i<20;i++){
         const product = new Product();
